728d9abf0665655683119d0ffcdf6d5b3a8f26b8
fyamashi2000 <105537888+fyamashi2000@users.noreply.github.com>
2022-05-14 22:23:07 +0900

    Update server.R

--- decon/server.R
+++ decon/server.R
@@ -32,8 +32,8 @@ shinyServer(function(input, output,session) {
     updateTabsetPanel(session, "tabs", selected = "Result")
   })
   
-#  session$onSessionEnded(function(){
-#    stopApp()
-#    q("no")
-#  })
+  session$onSessionEnded(function(){
+    stopApp()
+    q("no")
+  })
 })
